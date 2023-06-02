f50c507297b0a959947b75aa23bcc05fee739a9e
EC2 Default User <133302303+matsuno24@users.noreply.github.com>
2023-06-02 09:27:26 +0000

    [Finish]Kadai2_Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,20 @@
 class BooksController < ApplicationController
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully create."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all.order(id: :asc)
+      render :index
+    end
+  end
+  
   def index
     @books = Book.all
     @book = Book.new
   end
   
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end
-
   def show
     @book = Book.find(params[:id])
   end
@@ -17,27 +22,31 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
-  
+
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully update."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
-  
+
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
-  private
-  
+
+private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
 end
-
 
 
 #モデル名を付けるとき→単数形＆先頭は大文字(Book)
